f69deb0a5631ebf6d93068c6246dd5c1ff6dfe9a
defnotminhson <ms20070roblox@gmail.com>
2026-02-10 20:22:38 +0700

    Untested animationHandler

--- Utils/Game/AnimationHandler.py
+++ Utils/Game/AnimationHandler.py
@@ -1,21 +1,35 @@
 import pygame
 
-def to4Digits(n):
-    return str(n).zfill(4)
-
-class Animation: 
-    def __init__(self):
+class AnimationTrack: 
+    def __init__(self, sprite: pygame.sprite.Sprite):
         self.frames = []
         self.currentFrame = 0
+        self.frameCount = 0
+        self.sprite = sprite
 
     def loadAnimation(self, folder, frameCount):
         self.frames = []
+        self.frameCount = frameCount
         for i in range(0,frameCount):
             self.frames.append(pygame.image.load(f"{folder}/frame{frameCount:04d}"))
         return self.frames
     
-    def updateAnimation(self, frames, currentFrame, dt):
-        self.currentFrame += 1
-        self.image = frames[currentFrame]
+class Animator:
+    def __init__(self, dt: float, fps: int):
+        self.dt = dt
+        self.fps = fps
+
+    def playAnimation(self, animationTrack: AnimationTrack):
+        sprite = animationTrack.sprite 
+        frames = animationTrack.frames
+
+        currentFrame = animationTrack.currentFrame
+        if currentFrame == self.frameCount:
+            animationTrack.currentFrame = 0
+            currentFrame = 0
+
+        sprite.image = frames[currentFrame]
+        animationTrack.currentFrame += 1
+        
         
 
